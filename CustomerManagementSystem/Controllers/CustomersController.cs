ffdadd05c8fc9a54c72bbc7b4d9276d6d496f2b1
Ian <ian90911@gmail.com>
2018-07-08 22:32:21 +0800

    客戶資料CRUD & REPO STYLE CRUD

---
+++ CustomerManagementSystem/Controllers/CustomersController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CustomerManagementSystem.Models;
+
+namespace CustomerManagementSystem.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private I客戶資料Repository CustomerRepo;
+
+        public CustomersController()
+        {
+            this.CustomerRepo = RepositoryHelper.Get客戶資料Repository();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(CustomerRepo.All().ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = CustomerRepo.GetCustomerById(id.Value);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                CustomerRepo.Add(客戶資料);
+                CustomerRepo.UnitOfWork.Commit();
+                return RedirectToAction("Index");
+            }
+
+            return View(客戶資料);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = CustomerRepo.GetCustomerById(id.Value);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: Customers/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                CustomerRepo.UnitOfWork.Context.Entry(客戶資料).State = EntityState.Modified;
+                CustomerRepo.UnitOfWork.Commit();
+                return RedirectToAction("Index");
+            }
+            return View(客戶資料);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = CustomerRepo.GetCustomerById(id.Value);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶資料 客戶資料 = CustomerRepo.GetCustomerById(id);
+            CustomerRepo.Delete(客戶資料);
+            CustomerRepo.UnitOfWork.Commit();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                CustomerRepo.UnitOfWork.Context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- CustomerManagementSystem/Models/客戶資料Repository.cs
+++ CustomerManagementSystem/Models/客戶資料Repository.cs
@@ -6,11 +6,15 @@ namespace CustomerManagementSystem.Models
 {   
 	public  class 客戶資料Repository : EFRepository<客戶資料>, I客戶資料Repository
 	{
-
-	}
+        public 客戶資料 GetCustomerById(int id)
+        {
+            return this.All().FirstOrDefault(x => x.Id==id);
+        }
+    }
 
 	public  interface I客戶資料Repository : IRepository<客戶資料>
 	{
+        客戶資料 GetCustomerById(int id);
 
-	}
+    }
 }
