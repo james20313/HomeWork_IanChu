5ac2ab3455c2ad9ca482d0eeba56ad71efc1ab13
Ian <ian90911@gmail.com>
2018-07-08 23:28:22 +0800

    新增客戶銀行帳戶管理CRUD &REPO STYLE CRUD 新增客戶銀行帳戶管理選單連結

--- CustomerManagementSystem/Models/客戶銀行資訊Repository.cs
+++ CustomerManagementSystem/Models/客戶銀行資訊Repository.cs
@@ -6,11 +6,15 @@ namespace CustomerManagementSystem.Models
 {   
 	public  class 客戶銀行資訊Repository : EFRepository<客戶銀行資訊>, I客戶銀行資訊Repository
 	{
-
-	}
+        public 客戶銀行資訊 GetBankAccountById(int id)
+        {
+            return this.All().FirstOrDefault(x => x.Id == id);
+        }
+    }
 
 	public  interface I客戶銀行資訊Repository : IRepository<客戶銀行資訊>
 	{
+        客戶銀行資訊 GetBankAccountById(int id);
 
-	}
+    }
 }
