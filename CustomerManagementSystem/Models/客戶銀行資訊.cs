2092c9311f2514c01ac1afba024c1ee40358c274
Ian <ian90911@gmail.com>
2018-07-08 23:30:58 +0800

    修改客戶銀行帳號管理的刪除方式為FLAG式

--- CustomerManagementSystem/Models/客戶銀行資訊.cs
+++ CustomerManagementSystem/Models/客戶銀行資訊.cs
@@ -21,6 +21,7 @@ namespace CustomerManagementSystem.Models
         public Nullable<int> 分行代碼 { get; set; }
         public string 帳戶名稱 { get; set; }
         public string 帳戶號碼 { get; set; }
+        public bool IsDeleted { get; set; }
     
         public virtual 客戶資料 客戶資料 { get; set; }
     }
--- CustomerManagementSystem/Models/客戶銀行資訊Repository.cs
+++ CustomerManagementSystem/Models/客戶銀行資訊Repository.cs
@@ -10,6 +10,16 @@ namespace CustomerManagementSystem.Models
         {
             return this.All().FirstOrDefault(x => x.Id == id);
         }
+
+        public override IQueryable<客戶銀行資訊> All()
+        {
+            return base.All().Where(x=>x.IsDeleted==false);
+        }
+
+        public override void Delete(客戶銀行資訊 entity)
+        {
+            entity.IsDeleted = true;
+        }
     }
 
 	public  interface I客戶銀行資訊Repository : IRepository<客戶銀行資訊>
