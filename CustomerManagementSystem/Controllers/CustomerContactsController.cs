980780a096869109d6b3dc00e884f5ff0d8634e9
Ian <ian90911@gmail.com>
2018-07-08 23:06:03 +0800

    新增客戶聯絡人CRUD & REPO STYLE CRUD 新增客戶聯絡人連結

---
+++ CustomerManagementSystem/Controllers/CustomerContactsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CustomerManagementSystem.Models;
+
+namespace CustomerManagementSystem.Controllers
+{
+    public class CustomerContactsController : Controller
+    {
+        private I客戶聯絡人Repository ContactsRepo;
+        private I客戶資料Repository CustomerRepo;
+
+        public CustomerContactsController()
+        {
+            this.ContactsRepo = RepositoryHelper.Get客戶聯絡人Repository();
+            this.CustomerRepo = RepositoryHelper.Get客戶資料Repository(this.ContactsRepo.UnitOfWork);
+        }
+
+        // GET: CustomerContacts
+        public ActionResult Index()
+        {
+            var 客戶聯絡人 = ContactsRepo.All().Include(客 => 客.客戶資料);
+            return View(客戶聯絡人.ToList());
+        }
+
+        // GET: CustomerContacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = ContactsRepo.GetContactById(id.Value);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // GET: CustomerContacts/Create
+        public ActionResult Create()
+        {
+            ViewBag.客戶Id = new SelectList(CustomerRepo.All(), "Id", "客戶名稱");
+            return View();
+        }
+
+        // POST: CustomerContacts/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (ModelState.IsValid)
+            {
+                ContactsRepo.Add(客戶聯絡人);
+                ContactsRepo.UnitOfWork.Commit();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.客戶Id = new SelectList(CustomerRepo.All(), "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: CustomerContacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = ContactsRepo.GetContactById(id.Value);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.客戶Id = new SelectList(CustomerRepo.All(), "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // POST: CustomerContacts/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (ModelState.IsValid)
+            {
+                CustomerRepo.UnitOfWork.Context.Entry(客戶聯絡人).State = EntityState.Modified;
+                CustomerRepo.UnitOfWork.Commit();
+                return RedirectToAction("Index");
+            }
+            ViewBag.客戶Id = new SelectList(CustomerRepo.All(), "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: CustomerContacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = ContactsRepo.GetContactById(id.Value);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // POST: CustomerContacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶聯絡人 客戶聯絡人 = ContactsRepo.GetContactById(id);
+            ContactsRepo.Delete(客戶聯絡人);
+            ContactsRepo.UnitOfWork.Commit();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ContactsRepo.UnitOfWork.Context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- CustomerManagementSystem/Models/客戶聯絡人Repository.cs
+++ CustomerManagementSystem/Models/客戶聯絡人Repository.cs
@@ -6,11 +6,15 @@ namespace CustomerManagementSystem.Models
 {   
 	public  class 客戶聯絡人Repository : EFRepository<客戶聯絡人>, I客戶聯絡人Repository
 	{
-
-	}
+        public 客戶聯絡人 GetContactById(int id)
+        {
+            return this.All().FirstOrDefault(x => x.Id == id);
+        }
+    }
 
 	public  interface I客戶聯絡人Repository : IRepository<客戶聯絡人>
 	{
+        客戶聯絡人 GetContactById(int id);
 
-	}
+    }
 }
