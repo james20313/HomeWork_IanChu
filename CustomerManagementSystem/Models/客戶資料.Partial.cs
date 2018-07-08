12a5db3200f019bbe7ec37eeba918a1231ea3dd5
Ian <ian90911@gmail.com>
2018-07-08 22:42:03 +0800

    加入公司統一編號驗證屬性 客戶資料統編加入統一編號驗證

--- CustomerManagementSystem/Models/客戶資料.Partial.cs
+++ CustomerManagementSystem/Models/客戶資料.Partial.cs
@@ -1,14 +1,16 @@
 namespace CustomerManagementSystem.Models
 {
+    using Attribute;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(客戶資料MetaData))]
     public partial class 客戶資料
     {
+        
     }
-    
+
     public partial class 客戶資料MetaData
     {
         [Required]
@@ -20,6 +22,7 @@ namespace CustomerManagementSystem.Models
         
         [StringLength(8, ErrorMessage="欄位長度不得大於 8 個字元")]
         [Required]
+        [TaiwanCompanyNumberAttribute]
         public string 統一編號 { get; set; }
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
@@ -33,6 +36,7 @@ namespace CustomerManagementSystem.Models
         public string 地址 { get; set; }
         
         [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [EmailAddress]
         public string Email { get; set; }
     
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
