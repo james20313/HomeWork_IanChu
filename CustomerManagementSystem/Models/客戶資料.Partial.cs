c9db4f342595d14c93f87ad56f13d5ca7274539c
Ian <ian90911@gmail.com>
2018-07-13 23:17:05 +0800

    Add : 客戶類別 ENUM

--- CustomerManagementSystem/Models/客戶資料.Partial.cs
+++ CustomerManagementSystem/Models/客戶資料.Partial.cs
@@ -8,37 +8,43 @@ namespace CustomerManagementSystem.Models
     [MetadataType(typeof(客戶資料MetaData))]
     public partial class 客戶資料
     {
-        
     }
-
+    
     public partial class 客戶資料MetaData
     {
         [Required]
         public int Id { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         public string 客戶名稱 { get; set; }
-        
-        [StringLength(8, ErrorMessage="欄位長度不得大於 8 個字元")]
+
+        [StringLength(8, ErrorMessage = "欄位長度不得大於 8 個字元")]
         [Required]
         [TaiwanCompanyNumberAttribute]
         public string 統一編號 { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         public string 電話 { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         public string 傳真 { get; set; }
-        
-        [StringLength(100, ErrorMessage="欄位長度不得大於 100 個字元")]
+
+        [StringLength(100, ErrorMessage = "欄位長度不得大於 100 個字元")]
         public string 地址 { get; set; }
-        
-        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+
+        [StringLength(250, ErrorMessage = "欄位長度不得大於 250 個字元")]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+
+        [Required]
+        public CustomerTypeEnum 類別Id { get; set; }
     
+        public virtual 客戶類別 客戶類別 { get; set; }
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual ICollection<客戶聯絡人> 客戶聯絡人 { get; set; }
     }
--- CustomerManagementSystem/Models/客戶類別.Partial.cs
+++ CustomerManagementSystem/Models/客戶類別.Partial.cs
@@ -1,18 +1,32 @@
-using System.ComponentModel.DataAnnotations;
-
-[MetadataType(typeof(客戶類別MetaData))]
-public partial class 客戶類別
+namespace CustomerManagementSystem.Models
 {
-}
+    using Attribute;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
-public partial class 客戶類別MetaData
-{
-    [Required]
-    public int Id { get; set; }
-    
-    [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
-    [Required]
-    public string 類別名稱 { get; set; }
+    [MetadataType(typeof(客戶類別MetaData))]
+    public partial class 客戶類別
+    {
+    }
 
-    public virtual ICollection<客戶資料> 客戶資料 { get; set; }
+    public partial class 客戶類別MetaData
+    {
+        [Required]
+        public CustomerTypeEnum Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 類別名稱 { get; set; }
+
+        public virtual ICollection<客戶資料> 客戶資料 { get; set; }
+    }
+
+    public enum CustomerTypeEnum
+    {
+        一般客戶 = 1,
+        月結客戶 = 2,
+        VIP客戶 = 3,
+        到付應收客戶 = 4,
+    }
 }
