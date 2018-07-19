8bcc73f92204151deece2041f270b4d8585e7520
朱炳燁 Ian <ian.chu@miniasp.com>
2018-07-19 15:24:36 +0800

    Add : 建假資料用的SQL

---
+++ Database/FakeData/建立假客戶.sql
@@ -0,0 +1,10 @@
+﻿declare @i int=0;
+declare @length int =20;
+
+while @i <@length
+begin
+insert into [dbo].[客戶資料]
+([客戶名稱],[統一編號],[電話],[傳真],[地址],[Email],[IsDeleted],[類別Id])
+values('test'+cast(@i as nvarchar(2)),'51015548','23456789','29876543','test','test@abc.com',0,1);
+set @i=@i+1;
+end;
---
+++ Database/FakeData/建立假客戶聯絡人.sql
@@ -0,0 +1,11 @@
+﻿declare @i int=0;
+declare @length int =20;
+
+while @i <@length
+begin
+insert into [dbo].[客戶聯絡人]
+([客戶Id],[職稱],[姓名],[Email],[手機],[電話],[IsDeleted])
+--記得客戶ID要調成新建的
+values(1002,N'跑腿','test'+(cast(@i as nvarchar(2))),'test'+(cast(@i as nvarchar(2))+'@abc.com'),'0987654321','2987654321',0);
+set @i=@i+1;
+end;
