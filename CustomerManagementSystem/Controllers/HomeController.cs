7948369f435cbc781bc9d5d9bd02b8cd5a30b004
Ian <ian90911@gmail.com>
2018-07-27 00:33:02 +0800

    Add : 加入監控用ActionFilter
    可記錄每個 Action 與 ActionResult 執行的時間並輸出到 Debug 視窗

--- CustomerManagementSystem/Controllers/HomeController.cs
+++ CustomerManagementSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomerManagementSystem.Models.Attribute;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace CustomerManagementSystem.Controllers
 {
+    [OutputActionSpendTimeAttribute]
     public class HomeController : Controller
     {
         public ActionResult Index()
