5ab911c8db8e338f784f5832f0ec867d31f9b1e0
Ian <ian90911@gmail.com>
2018-07-28 00:33:50 +0800

    Add : 範本檢視 加入電話、EMAIL與地址的顯示範本

--- CustomerManagementSystem/ViewModels/CustometContactViewModel.cs
+++ CustomerManagementSystem/ViewModels/CustometContactViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -23,12 +24,15 @@ namespace CustomerManagementSystem.ViewModels
         public string 姓名 { get; set; }
 
         [DisplayName("Email")]
+        [UIHint("EmailAddress")]
         public string Email { get; set; }
 
         [DisplayName("手機")]
+        [UIHint("PhoneNumber")]
         public string 手機 { get; set; }
 
         [DisplayName("電話")]
+        [UIHint("PhoneNumber")]
         public string 電話 { get; set; }
     }
 }
